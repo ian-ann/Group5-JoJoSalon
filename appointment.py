f6c29fee1940fc420e84e5d9e2545e48d63b3c7b
KristelFaye09 <152124583+KristelFaye09@users.noreply.github.com>
2023-12-07 14:00:47 -0700

    Update appointment.py
    appointment class

--- appointment.py
+++ appointment.py
@@ -1,32 +1,77 @@
-class Appointment :
-    def __init__(self) -> None:
-        pass
-    def get_client_name():
-        pass
-    def get_client_phone():
-        pass
-    def get_appt_type():
-        pass
-    def get_appt_day():
-        pass
-    def get_appt_start_time():
-        pass
-    def get_app_type_desc():
-        pass
-    def get_end_time_hour():
-        pass
-    def set_client_name():
-        pass
-    def set_client_phone():
-        pass
-    def set_appt_type():
-        pass
-    def schedule():
-        pass
-    def cancel():
-        pass
-    def format_record():
-        pass
-    def __str__(self) -> str:
-        pass
+class Appointment:
+
+    def __init__(self, day_of_week, start_time_hour):
+        self.client_name = ""
+        self.client_phone = ""
+        self.appt_type = 0
+        self.day_of_week = day_of_week
+        self.start_time_hour = start_time_hour
+
+    def get_client_name(self):
+        return self.client_name
+    
+    def get_client_phone(self):
+        return self.client_phone
+    
+    def get_appt_type(self):
+        return self.appt_type
+    
+    def get_day_of_week(self):
+        return self.day_of_week
+    
+    def get_start_time_hour(self):
+        return self.start_time_hour
+    
+    def get_appt_type_desc(self):
+        if self.appt_type == 0:
+            return 'Available'
+        elif self.appt_type == 1:
+            return 'Mens Cut'
+        elif self.appt_type == 2:
+            return 'Ladies Cut'
+        elif self.appt_type == 3:
+            return 'Mens Colouring'
+        elif self.appt_type == 4:
+            return 'Ladies Colouring'
+        else:
+            return 'Unknown'
+        
+    #def get_appt_type_desc(self):
+    #description = ['Available', 'Mens Cut', 'Ladies Cut', 'Mens Colouring', 'Ladies Colouring']
+    #return descriptions[self.appt_type] 
+    #if 0 <= self.appt_type < len(descriptions) else 'Unknown'
+
+    def get_end_time_hour(self): 
+        return self.start_time_hour + 1
+    
+    def set_client_name(self, client_name):
+        self.client_name = client_name
+
+    def set_client_phone(self, client_phone):
+        self.client_phone = client_phone
+
+    def set_appt_type(self, appt_type):
+        self.appt_type = appt_type
+
+    def schedule(self, client_name, client_phone, appt_type):
+        self.set_client_name(client_name)
+        self.set_client_phone(client_phone)
+        self.set_appt_type(appt_type)
+
+    def cancel(self):
+        self.client_name = ""
+        self.client_phone = ""
+        self.appt_type = 0
+
+    def format_record(self):
+        format_str = '{},{},{},{:02d}'.format(self.client_name, self.client_phone, self.day_of_week, self.start_time_hour)
+        return format_str
+    
+    def __str__(self):
+        format_str = f'{self.client_name}'
+        format_str += f'{self.client_phone:>15s}'
+        format_str += f'{self.day_of_week:10s}'
+        format_str += f'{self.start_time_hour:02d}:00 - {self.get_end_time_hour():02d}:00\n'
+        format_str += f'{self.get_appt_type_desc():>20s}'
+        return format_str
         
